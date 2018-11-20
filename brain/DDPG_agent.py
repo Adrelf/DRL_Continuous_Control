521506160e61fe17d806e96c56d6766f00548891
Adrien Elfassi <44059923+Adrelf@users.noreply.github.com>
2018-11-20 10:44:06 +0100

    Add files via upload
    Update all the files in this folder

--- brain/DDPG_agent.py
+++ brain/DDPG_agent.py
@@ -8,12 +8,13 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 BUFFER_SIZE = int(1e6)  # replay buffer size
-BATCH_SIZE = 128  # minibatch size
-GAMMA = 0.99  # discount factor
-TAU = 1e-3  # for soft update of target parameters
-LR_ACTOR = 1e-3  # learning rate of the actor
-LR_CRITIC = 3e-4  # learning rate of the critic
-WEIGHT_DECAY = 0.0001  # L2 weight decay
+BATCH_SIZE = 1024       # minibatch size
+GAMMA = 0.99            # discount factor
+TAU = 1e-3              # for soft update of target parameters
+LR_ACTOR = 1e-4         # learning rate of the actor
+LR_CRITIC = 1e-3        # learning rate of the critic
+WEIGHT_DECAY = 0        # L2 weight decay
+NUM_AGENTS = 20         # Number of agents
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -45,24 +46,26 @@ class Agent():
     self.critic_optimizer = optim.Adam(self.critic_local.parameters(), lr=LR_CRITIC, weight_decay=WEIGHT_DECAY)
 
     # Noise process
-    self.noise = OUNoise(action_size, random_seed)
+    self.noise = OUNoise((NUM_AGENTS,action_size), random_seed)
 
     # Replay memory
     self.memory = ReplayBuffer(action_size, BUFFER_SIZE, BATCH_SIZE, random_seed)
 
-  def step(self, state, action, reward, next_state, done):
+  def step(self, states, actions, rewards, next_states, dones, t):
     """Save experience in replay memory, and use random sample from buffer to learn."""
     # Save experience / reward
-    self.memory.add(state, action, reward, next_state, done)
+    for state, action, reward, next_state, done in zip(states, actions, rewards, next_states, dones):
+      self.memory.add(state, action, reward, next_state, done)
 
     # Learn, if enough samples are available in memory
-    if len(self.memory) > BATCH_SIZE:
-      experiences = self.memory.sample()
-      self.learn(experiences, GAMMA)
+    if len(self.memory) > BATCH_SIZE and t % 20 == 0:
+      for i in range(10):
+        experiences = self.memory.sample()
+        self.learn(experiences, GAMMA)
 
   def act(self, state, add_noise=True):
     """Returns actions for given state as per current policy."""
-    state = torch.from_numpy(state).float().unsqueeze(0).to(device)
+    state = torch.from_numpy(state).float().to(device)
     self.actor_local.eval()
     with torch.no_grad():
       action = self.actor_local(state).cpu().data.numpy()
@@ -99,6 +102,7 @@ class Agent():
     # Minimize the loss
     self.critic_optimizer.zero_grad()
     critic_loss.backward()
+    torch.nn.utils.clip_grad_norm(self.critic_local.parameters(), 1)
     self.critic_optimizer.step()
 
     # ---------------------------- update actor ---------------------------- #
@@ -135,6 +139,7 @@ class OUNoise:
     self.mu = mu * np.ones(size)
     self.theta = theta
     self.sigma = sigma
+    self.size = size
     self.seed = random.seed(seed)
     self.reset()
 
@@ -145,7 +150,7 @@ class OUNoise:
   def sample(self):
     """Update internal state and return it as a noise sample."""
     x = self.state
-    dx = self.theta * (self.mu - x) + self.sigma * np.array([random.random() for i in range(len(x))])
+    dx = self.theta * (self.mu - x) + self.sigma * np.random.standard_normal(self.size)
     self.state = x + dx
     return self.state
 
